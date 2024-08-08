c221c7f5c8b81a156f5d312a6a5fe6ccbe100508
Echin-h <23050118@hdu.edu.cn>
2024-08-08 15:44:26 +0800

    fix: main

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	helloV1 "github.com/Echin-h/grpc-template-proto/gen/proto/hello/v1"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+type HelloService struct {
+	helloV1.UnimplementedGreeterServiceServer
+}
+
+var _ helloV1.GreeterServiceServer = (*HelloService)(nil)
 
 func main() {
-	fmt.Println("Hello, World!")
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+	grpcServer := grpc.NewServer(grpc.MaxRecvMsgSize(1024 * 1024 * 1024))
+	helloV1.RegisterGreeterServiceServer(&grpc.Server{}, &HelloService{})
+	log.Fatal(grpcServer.Serve(lis))
 }
